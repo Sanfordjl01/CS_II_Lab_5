d2534a5e1c2b7b0cefa974aae1709f9867cff78c
chadhirsch <chad.a.hirsch@wmich.edu>
2017-03-16 15:53:48 -0400

    Update LA5Main.java

--- CS1120LA5/src/edu/wmich/cs1120/la5/LA5Main.java
+++ CS1120LA5/src/edu/wmich/cs1120/la5/LA5Main.java
@@ -4,11 +4,8 @@ public class LA5Main {
 
 	public static void main(String[] args) {
 		int i = 0;
-<<<<<<< HEAD
-		
 	System.out.print("Hello");
-=======
->>>>>>> branch 'master' of https://github.com/Sanfordjl01/CS_II_Lab_5
+
 	}
 
 }
