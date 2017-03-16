90c415caec46f75e3fa43f811508498cc992c69f
Jon <Jon@10.80.144.145>
2017-03-16 15:21:13 -0400

    Added a String

--- CS1120LA5/src/edu/wmich/cs1120/la5/LA5Main.java
+++ CS1120LA5/src/edu/wmich/cs1120/la5/LA5Main.java
@@ -3,8 +3,9 @@ package edu.wmich.cs1120.la5;
 public class LA5Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 0;
+		
+		String y = "Flo";
 	}
 
 }
