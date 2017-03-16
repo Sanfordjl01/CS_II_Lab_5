18f2a6a0d9e3e01b2c2fdb2da6b2fddc04854469
Jonathan Sanford <jonathan.l.sanford@wmich.edu>
2017-03-16 15:55:36 -0400

    commented something

--- CS1120LA5/src/edu/wmich/cs1120/la5/LA5Main.java
+++ CS1120LA5/src/edu/wmich/cs1120/la5/LA5Main.java
@@ -4,11 +4,11 @@ public class LA5Main {
 
 	public static void main(String[] args) {
 		int i = 0;
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		
 	System.out.print("Hello");
-=======
->>>>>>> branch 'master' of https://github.com/Sanfordjl01/CS_II_Lab_5
+//=======
+//>>>>>>> branch 'master' of https://github.com/Sanfordjl01/CS_II_Lab_5
 	}
 
 }
